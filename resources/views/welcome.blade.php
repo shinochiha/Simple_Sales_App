6747de6fd8b8f6ce5b6fe92a9ae0ac76ce00e5ab
Fitra Aziz <fitradevelopers71@gmail.com>
2018-11-07 10:30:30 +0700

    Api V1.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <h1 align="center" style="background-color:blue;color:white;padding:2%;margin-bottom:4%;" class="shadow">API - Simple Sales App</h1>
         <h2 align="center" style="color:white;padding:1%;margin-left:43%;margin-right:43%;" class="rounded shadow bg-dark">Backend (2)</h2>
         <br>
-        <p align="center">The master project repository is <a href="#">github.com/shinochiha</a></p>
+        <p align="center">The master project repository is <a href="https://github.com/shinochiha/Simple_Sales_App">github.com/shinochiha/Simple_Sales_App</a></p>
 
         <br>
         <table border="2" cellpadding="15" align="center" class="shadow">
@@ -22,27 +22,27 @@
 
             <tr>
                 <td>Customers</td>
-                <td><a href="https://fitraabackend2.herokuapp.com/api/v1/customers" target="_blank">https://fitraabackend2.herokuapp.com/api/v1/customers</a></td>
+                <td><a href="https://simplesalesapp.herokuapp.com/api/v1/customers" target="_blank">https://simplesalesapp.herokuapp.com/api/v1/customers</a></td>
                 <td>GET, POST, PATCH/PUT, DELETE</td>
             </tr>
 
             <tr>
                 <td>Products</td>
-                <td><a href="https://fitraabackend2.herokuapp.com/api/v1/products" target="_blank">https://fitraabackend2.herokuapp.com/api/v1/products</a></td>
+                <td><a href="https://simplesalesapp.herokuapp.com/api/v1/products" target="_blank">https://simplesalesapp.herokuapp.com/api/v1/products</a></td>
                 <td>GET, POST, PATCH/PUT, DELETE</td>
 
             </tr> 
 
             <tr>
                 <td>Sales</td>
-                <td><a href="https://fitraabackend2.herokuapp.com/api/v1/sales" target="_blank">https://fitraabackend2.herokuapp.com/api/v1/sales</a></td>
+                <td><a href="https://simplesalesapp.herokuapp.com/api/v1/sales" target="_blank">https://simplesalesapp.herokuapp.com/api/v1/sales</a></td>
                 <td>GET, POST, PATCH/PUT, DELETE</td>
 
             </tr> 
 
             <tr>
                 <td>Categories</td>
-                <td><a href="https://fitraabackend2.herokuapp.com/api/v1/categories" target="_blank">https://fitraabackend2.herokuapp.com/api/v1/categories</a></td> 
+                <td><a href="https://simplesalesapp.herokuapp.com/api/v1/categories" target="_blank">https://simplesalesapp.herokuapp.com/api/v1/categories</a></td> 
                 <td>GET, POST, PATCH/PUT, DELETE</td>
                 
             </tr> 
@@ -53,8 +53,5 @@
             &copy; Fitra Aziz AL - Rasyid
         </footer>
           
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
         </body>
 </html>
